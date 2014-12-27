8f240e92faa1cd848c454b83aae12d03144282ad
karino <hogeika2@gmail.com>
2014-12-27 23:50:16 +0900

    Fix gamma collection.

--- app/src/main/rs/fourbitgray.rs
+++ app/src/main/rs/fourbitgray.rs
@@ -10,8 +10,8 @@ uchar4 __attribute__((kernel)) fourBitGrayWithGamma(uchar4 in)
     float gray = (2*in.r+4*in.g+in.b)/7.0f;
     float withGamma =pow(gray/255.0f, gammaValue)*255.0f;
     float grayVal = clamp(withGamma, .0f, 255.0f);
-    // for 4bit color, 0-16 =>0 17-33=>17, ...
-    int bitLevel = (int)(grayVal/17.0f);
+    // for 4bit color, 0-15 =>0 16-31=>17, ...
+    int bitLevel = (int)(grayVal/16.0f);
     uchar4 outVal;
     outVal.r = (uchar)rsClamp(17*bitLevel, 0, 255);
     outVal.g = (uchar)rsClamp(17*bitLevel, 0, 255);
